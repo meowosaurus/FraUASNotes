cfcb4053e436dbd099c14e41a57581a13aff1527
Vynalys <maximiliannrabe@gmail.com>
2022-05-19 11:23:06 +0200

    Code cleanup + Toolbar added

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,18 +1,35 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QGridLayout, QVBoxLayout, QTextEdit, QMenuBar, QMenu
+from PySide6.QtCore import QSize
+from PySide6.QtGui import Qt
+from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QGridLayout, QVBoxLayout, QTextEdit, QMenuBar, QMenu, \
+    QToolBar
 
 
 class GUI(QWidget):
+
     def __init__(self):
         super().__init__()
         self.resize(1920, 1080)
         self.setWindowTitle("FraUasNotes")
 
+        self.create_menu()
+        self.create_textbox_1()
+        self.create_textbox_2()
+        self.create_toolbar_horizontal()
+        self.create_toolbar_vertical()
+
         main_layout = QGridLayout()  # Main Layout
+        main_layout.setMenuBar(self.menubar)
+        main_layout.addWidget(self.tabs1, 2, 2)
+        main_layout.addWidget(self.tabs2, 2, 3)
+        main_layout.addWidget(self.toolbar_hori, 1, 0)
+        main_layout.addWidget(self.toolbar_vert, 0, 0)
+
+        self.setLayout(main_layout)  # sets Layout
+
+    def create_textbox_1(self):
         textbox = QTextEdit()
-        textbox_2 = QTextEdit()
-        menubar = QMenuBar()
 
         self.tb_1 = QWidget()  # Creates tb1 as widget
         self.tb_1.layout = QVBoxLayout()  # Creates layout of tab1_1
@@ -20,22 +37,30 @@ class GUI(QWidget):
         self.tb_1.setLayout(self.tb_1.layout)
         self.tabs1 = QTabWidget()  # creates tabwidget
         self.tabs1.addTab(self.tb_1, 'Tab 1')  # adds tab1_1 to tab1 + description
-        main_layout.addWidget(self.tabs1, 1, 0) #adds Widget to mainLayout
 
-        self.tb_2 = QWidget()                   #Same functionality as Widget above
+    def create_textbox_2(self):
+        self.textbox = QTextEdit()
+        self.tb_2 = QWidget()  # Same functionality as Widget above
         self.tb_2.layout = QVBoxLayout()
-        self.tb_2.layout.addWidget(textbox_2)
+        self.tb_2.layout.addWidget(self.textbox)
         self.tb_2.setLayout(self.tb_2.layout)
         self.tabs2 = QTabWidget()
         self.tabs2.addTab(self.tb_2, 'Tab 2')
-        main_layout.addWidget(self.tabs2, 1, 1)
 
-        fileMenu = QMenu("&File", self)         #adds Menubar
-        menubar.addMenu(fileMenu)
-        main_layout.addWidget(menubar, 0, 0)    #adds Menubar to Layout
+    def create_menu(self):
+        self.menubar = QMenuBar()  # adds Menubar
 
+        self.file_menu = QMenu("&File", self)
+        self.menubar.addMenu(self.file_menu)
 
-        self.setLayout(main_layout)  # sets Layout
+    def create_toolbar_horizontal(self):
+        self.toolbar_hori = QToolBar("Yes")
+
+    def create_toolbar_vertical(self):
+        self.toolbar_vert = QToolBar("Yes")
+        self.toolbar_vert.setOrientation(Qt.Vertical)
+        self.toolbar_vert.setIconSize(QSize(16,16))
+
 
 
 if __name__ == '__main__':  # Main for testing purposes
